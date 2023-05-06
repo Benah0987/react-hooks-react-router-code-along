88eb3ab6ff57219c166379c8bdcaa8efe83084f0
Benah0987 <wanyoikebenayah77@gmail.com>
2023-05-06 20:55:11 +0300

    added a Login component

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+
+function App() {
+    return (
+      <div>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </div>
+    );
+  }
+  
+  export default App;
